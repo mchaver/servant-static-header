b1bfd3b819fd0ec5ee85c4d10aa704a9799e1ef9
James Haver <mchaver@gmail.com>
2016-12-05 10:50:59 +0800

    Test for multiple StaticHeaders

--- servant-client-static-header/test/Spec.hs
+++ servant-client-static-header/test/Spec.hs
@@ -30,10 +30,12 @@ testAPI = Proxy
 app :: Application
 app = serve testAPI testServer
 
-type TestAPI = "test" :> StaticHeader "User-Agent" "Hspec" :> Get '[JSON] Text
+type TestAPI = "single"   :> StaticHeader "User-Agent" "Hspec" :> Get '[JSON] Text
+          :<|> "multiple" :> StaticHeader "User-Agent" "Hspec" :> StaticHeader "TestData" "abc" :> Get '[JSON] [Text]
 
 getStaticHeader :: ClientM Text
-getStaticHeader = client testAPI
+getMultipleStaticHeaders :: ClientM [Text]
+getStaticHeader :<|> getMultipleStaticHeaders = client testAPI
 
 {-
 type TT = "x" :> Get '[JSON] Text
@@ -47,26 +49,38 @@ getTT = client ttAPI
 
 -- test server
 testServer :: Server TestAPI
-testServer = getStaticHeaderH
+testServer = getStaticHeaderH :<|> getMultipleStaticHeadersH
   where
     getStaticHeaderH h = liftIO $ getStaticHeader h
+    getMultipleStaticHeadersH h1 h2 = liftIO $ getMultipleStaticHeaders h1 h2
     
     getStaticHeader :: Maybe Text -> IO Text
     getStaticHeader (Just h) = return h
     getStaticHeader _ = undefined
 
+    getMultipleStaticHeaders :: Maybe Text -> Maybe Text -> IO [Text]
+    getMultipleStaticHeaders (Just h1) (Just h2) = return [h1,h2]
+    getMultipleStaticHeaders _ _ = undefined
+
 spec :: Spec
 spec = do
   runIO $ forkIO $ runSettings (setPort 3000 $ defaultSettings) app
   manager <- runIO $ newManager defaultManagerSettings
-  describe "" $ do
-    it "" $ do
+  describe "StaticHeader" $ do
+    it "gets the static header it passes" $ do
       res <- runClientM getStaticHeader (ClientEnv manager (BaseUrl Http "localhost" 3000 ""))
       case res of 
         Left err -> do
           putStrLn $ "Error: " ++ show err
           True `shouldBe` False
         Right h -> h `shouldBe` "Hspec"
+    it "gets the multiple static headers it passes" $ do
+      res <- runClientM getMultipleStaticHeaders (ClientEnv manager (BaseUrl Http "localhost" 3000 ""))
+      case res of 
+        Left err -> do
+          putStrLn $ "Error: " ++ show err
+          True `shouldBe` False
+        Right h -> h `shouldBe` ["Hspec","abc"]
         
 
 main :: IO ()
